4f1025f05c81d93070605ee5b2c5baf27b5df424
SergeiDko <msf77@mail.ru>
2023-11-16 16:41:13 +0100

    Добавил кнопку и лейбл, подключил счётчик.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet weak var buttonCounter: UIButton!
+    var counter = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        labelCounter.text = "Значение сётчика: \(String(counter))"
 
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        labelCounter.text = "Значение сётчика: \(String(counter))"
+    }
+    
 
 }
 
